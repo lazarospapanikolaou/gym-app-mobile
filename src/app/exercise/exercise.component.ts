ae3d5a0708f2691e54b2b0a956d72e936d8cd806
chrisboursis <34278531+chrisboursis@users.noreply.github.com>
2024-04-14 18:02:59 +0300

    Working on slider-confirm

--- src/app/exercise/exercise.component.ts
+++ src/app/exercise/exercise.component.ts
@@ -5,12 +5,19 @@ import {
   IonTitle,
   IonContent,
 } from '@ionic/angular/standalone';
+import { SliderConfirmComponent } from '../components/slider-confirm/slider-confirm.component';
 
 @Component({
   selector: 'app-exercise',
   templateUrl: './exercise.component.html',
   styleUrls: ['./exercise.component.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    SliderConfirmComponent,
+  ],
   standalone: true,
 })
 export class ExerciseComponent implements OnInit {
