16e96d731a77daa4a9a93883ddfb2ced4f6fa78b
LazarosPap <l.papanikolaou@pragma-iot.com>
2024-04-22 14:49:52 +0300

    Update

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,21 +1,58 @@
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+
+import { Validators,FormBuilder, FormGroup, AbstractControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonIcon, IonButton, IonCol, IonRow, IonGrid, IonNote, IonInput, IonItem, IonList, IonHeader, IonCardSubtitle, IonToolbar, IonCardContent, IonCardHeader, IonContent, IonCard, IonCardTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonButton, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule,ReactiveFormsModule ,FormsModule ,IonCardTitle, IonCard, IonContent, IonCardHeader, IonCardContent, IonToolbar, IonCardSubtitle, IonHeader, IonIcon, IonButton, IonCol, IonRow, IonGrid, IonNote, IonInput, IonItem, IonList, ]
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  showPassword = false;
+
+
+  loginForm: FormGroup = this.fb.group({
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ],
+    ],
+    password: ['', Validators.required],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
+
+  get email(): AbstractControl | null {
+    return this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl | null {
+    return this.loginForm.get('password');
+  }
+
+  getType(): string {
+    return this.showPassword ? 'text' : 'password';
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log('I AM IN LOGIN')
   }
 
 }
