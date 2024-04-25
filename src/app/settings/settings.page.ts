41f9450f4a1dc96bbe9dbe42261553efee70bf96
LazarosPap <l.papanikolaou@pragma-iot.com>
2024-04-25 16:50:58 +0300

    Update

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,24 +1,41 @@
+/* eslint-disable @angular-eslint/use-lifecycle-interface */
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCol, IonGrid, IonRow, IonButton, IonIcon, IonList, IonListHeader, IonLabel, IonItem } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCol, IonGrid, IonRow, IonButton, IonIcon, IonList, IonListHeader, IonLabel, IonItem, IonSpinner } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
 import { addIcons } from 'ionicons';
 import { logOutOutline } from 'ionicons/icons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-settings',
   templateUrl: 'settings.page.html',
   styleUrls: ['settings.page.scss'],
   standalone: true,
-  imports: [IonItem, IonLabel, IonListHeader, IonList, IonIcon, IonButton, IonRow, IonGrid, IonCol, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [CommonModule, IonSpinner, IonItem, IonLabel, IonListHeader, IonList, IonIcon, IonButton, IonRow, IonGrid, IonCol, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class SettingsPage {
+  loading: boolean = true;
+
   constructor(private userService: UserService, private router: Router) {
     addIcons({ logOutOutline });
   }
 
+  ngOnInit(){
+    this.getReload();
+  }
 
+  ionViewWillEnter() {
+    this.getReload();
+  }
+
+  getReload(){
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+    }, 500)
+  }
 
   async logout(): Promise<void> {
     await this.userService.purgeAuth();
